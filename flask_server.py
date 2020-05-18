88fce5288f09bc52d552986013dcf5adaf0f8ca1
kokot300 <kokot300@gmail.com>
2020-05-18 20:52:37 +0200

    code refractorization

--- flask_server.py
+++ flask_server.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def root():
+    '''
+    flask server that can guess number you pick in max 10 attempts.
+    :return: rendered html template
+    '''
     if request.method == 'GET':
         game_return = g.game()
         my_guess = int(game_return[0])
@@ -19,10 +23,12 @@ def root():
         max = request.form.get('max')
         answer = request.form.get('answer')
         print(min, max, answer)
+        if answer == None:
+            answer = min
         game_return = g.game(int(answer), int(min), int(max))
         print(game_return)
         if game_return == 'wygrałem!':
-            msg=game_return
+            msg = game_return
             return render_template('form.html', msg=msg, my_guess=500, min=0, max=1000)
         my_guess = int(game_return[0])
         min = int(game_return[1])
--- game.py
+++ game.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 def game(where_we_are_flag=0, min=0, max=1000):
+    '''
+
+    :param where_we_are_flag:
+    :param min: minimum number from range
+    :param max: maximum number of the range
+    :return: int which is number that the program is going to guess
+    '''
     guess = int((max - min) / 2) + int(min)
     if where_we_are_flag == 0:
         guess = int((max - min) / 2) + int(min)
         return guess, min, max
-    if where_we_are_flag == 1:
+    elif where_we_are_flag == 1:
         return 'wygrałem!'
     elif where_we_are_flag == 2:
         max = guess
@@ -15,5 +22,5 @@ def game(where_we_are_flag=0, min=0, max=1000):
         min = guess
         guess = int((max - min) / 2) + int(min)
         return guess, min, max
-    else:
+    else:  # just messing up. sorry for that ;)
         raise BlockingIOError
